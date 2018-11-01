020ff8d3c4db70c3842ebb29d022f8a76184a17d
mmiah <mmiah00@stuy.edu>
2018-11-01 19:46:52 -0400

    overid set and both add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 public class NoNullArrayList<T> extends ArrayList <T>{
   public NoNullArrayList (){
     super();
@@ -9,7 +8,33 @@ public class NoNullArrayList<T> extends ArrayList <T>{
     super(startingCapacity);
   }
 
+  public T set (int index, T value) {
+    if (value == null) {
+      throw new IllegalArgumentException ("Can't set a null");
+    }
+    else {
+      super.set (index, value);
+    }
+    return value;
+  }
 
+  public void add (int index, T value) {
+    if (value == null) {
+      throw new IllegalArgumentException ("Can't add a null");
+    }
+    else {
+      super.add (index, value);
+    }
+  }
 
-
+  public boolean add (T value) {
+    if (value == null) {
+       throw new IllegalArgumentException ("Can't add a null");
+    }
+    else {
+      super.add (value);
+    }
+    return true;
+  }
+  
 }
