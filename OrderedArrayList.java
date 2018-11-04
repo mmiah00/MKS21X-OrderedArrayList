79341faf6f1aeb2cbd830abd2203608018d95bf9
mmiah <mmiah00@stuy.edu>
2018-11-04 17:42:22 -0500

    wrote bigger (T,T) to compare two elements and started writing sort to order the array

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,18 +9,52 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super (startingCapacity);
   }
 
-  public void add (T value) {
+  public void add (int index, T value) {
+    super.add (index, value);
   }
 
   public boolean add (T value)  {
-    return true;
+    super.add (value);
   }
 
-  public boolean compareTo (T val1, T val2) {
-     return false;
+  private boolean bigger (T val1, T val2) {
+     // returns if first param is greater than second param
+     return (val1.compareTo (val2) > 0);
   }
-  
+
+  private void sort (T value) {
+    //function to sort the array
+    int x = 0;
+    while (x < size ()) {
+      if (compare (value, get (x))) {
+        super.add (x, value);
+        x = size ();
+      }
+      else {
+        x += 1;
+      }
+    }
+  }
+
+
+/*
+  private void sort () {
+    int x = 0;
+    while (x < size ())  {
+      T now = get (x);
+      T next = get (x + 1);
+      if (! bigger (now, next)) {
+        set (x, next);
+        set (x + 1, now);
+      }
+      x += 1;
+    }
+  }
+*/
+
   public T set (int index, T value) {
     super.set (index, value);
+
   }
+
 }
